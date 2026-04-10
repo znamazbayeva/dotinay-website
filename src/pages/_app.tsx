0afbc10d6a6f9b3aa05c02bc1a56f112c71f487e
Samatqyzy <zhuldyznamazbayeva@gmail.com>
2026-04-10 21:43:49 +0100

    fix(app): fix loader to get the url previews

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,21 +2,16 @@ import { ThemeProvider, CssBaseline, Box } from "@mui/material";
 import theme from "../theme";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import { IBM_Plex_Mono, Patrick_Hand } from "next/font/google";
+import { Patrick_Hand } from "next/font/google";
 import { useState, useEffect } from "react";
 import Loader from "../components/Loader";
-
-const plexMono = IBM_Plex_Mono({
-  subsets: ["latin"],
-  weight: ["400", "600", "700"],
-  display: "swap",
-});
 
 const patrickHand = Patrick_Hand({
   subsets: ["latin"],
   weight: "400",
   display: "swap",
 });
+
 export default function MyApp({ Component, pageProps }: any) {
   const [loading, setLoading] = useState(true);
 
@@ -25,26 +20,38 @@ export default function MyApp({ Component, pageProps }: any) {
     return () => clearTimeout(timer);
   }, []);
 
-  if (loading) return <Loader />;
-
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-        <Box
-         className={patrickHand.className}
-          display="flex"
-          flexDirection="column"
-          minHeight="100vh"
-          sx={{
-            background:   "radial-gradient(circle at top left, #FFF7ED 0%, transparent 28%), radial-gradient(circle at top right, #E0F2FE 0%, transparent 30%), linear-gradient(to bottom, #F8FBFF 0%, #F3F8FF 100%)",
-            backgroundAttachment: "fixed",
-          }}
-        >
+      <Box
+        className={patrickHand.className}
+        display="flex"
+        flexDirection="column"
+        minHeight="100vh"
+        sx={{
+          background:
+            "radial-gradient(circle at top left, #FFF7ED 0%, transparent 28%), radial-gradient(circle at top right, #E0F2FE 0%, transparent 30%), linear-gradient(to bottom, #F8FBFF 0%, #F3F8FF 100%)",
+          backgroundAttachment: "fixed",
+          position: "relative",
+        }}
+      >
         <Navbar />
         <Box component="main" flexGrow={1}>
           <Component {...pageProps} />
         </Box>
         <Footer />
+
+        {loading && (
+          <Box
+            sx={{
+              position: "fixed",
+              inset: 0,
+              zIndex: 9999,
+            }}
+          >
+            <Loader />
+          </Box>
+        )}
       </Box>
     </ThemeProvider>
   );
